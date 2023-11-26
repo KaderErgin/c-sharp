ae93cd205d117235a9bb5668c2f3e97e82d748c1
Kader Ergin <kader.ergin.37@gmail.com>
2023-11-27 00:16:58 +0300

    Update question_3.cs

--- Algorithm_Questions/question_3.cs
+++ Algorithm_Questions/question_3.cs
@@ -1,1 +1,34 @@
+using System;
 
+namespace algorithmQuestions
+{
+    class ucuncuSoru
+    {
+        static void Main(string[] args)
+        {
+            /*Soru 3: Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). 
+           Sonrasında kullanıcıdan n adet kelime girmesi isteyin. 
+           Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın. */
+
+            Console.Write("Sıralanmasını istediğiniz kelime sayısını giriniz: ");
+            int n = int.Parse(Console.ReadLine());
+            string[] dizi = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Lütfen " + (i + 1) + ".kelimeyi giriniz:");
+                dizi[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Girdiğiniz kelimelerin sıralanışı aşağıdaki gibidir: ");
+            Array.Reverse(dizi);
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.WriteLine(dizi[i]);
+            }
+            
+        }
+
+    }
+
+}
